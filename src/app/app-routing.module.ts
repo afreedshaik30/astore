f062b12d86968410cb03a14d06b8d29e5c727cfc
Afreed Shaik <skafrid482@gmail.com>
2023-08-14 13:53:09 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'seller-auth',
+    component:SellerAuthComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
